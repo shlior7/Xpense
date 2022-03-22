e28e66f845cdf019affa7e63c4e6a714a0bdf46c
Lior Shtaimberg <liorsht@wix.com>
2022-03-22 13:46:19 +0200

    Register the House class to the admin managing UI

--- house/models.py
+++ house/models.py
@@ -1,5 +1,5 @@
+import uuid
 from django.db import models
-from django.core.validators import MinValueValidator
 
 
 class Country(models.Model):
@@ -18,33 +18,30 @@ class City(models.Model):
 
 
 class Job(models.TextChoices):
-    TEACHER = 'Teacher'
-    STUDENT = 'Student'
-    PROGRAMMER = 'Programmer'
-    ARTIST = 'Artist'
-    MANAGER = 'Manager'
-    ARMY = 'Army'
-    POLICE = 'Police'
-    DOCTOR = 'Doctor'
-    VET = 'Vet'
-    NURSE = 'Nurse'
-    TECHNICHIAN = 'Technichian'
-    CLEANER = 'Cleaner'
-    OTHER = 'Other'
-    UNEMPLOYED = 'Unemployed'
+    TEACHER = "Teacher"
+    STUDENT = "Student"
+    PROGRAMMER = "Programmer"
+    ARTIST = "Artist"
+    MANAGER = "Manager"
+    ARMY = "Army"
+    POLICE = "Police"
+    DOCTOR = "Doctor"
+    VET = "Vet"
+    NURSE = "Nurse"
+    TECHNICHIAN = "Technichian"
+    CLEANER = "Cleaner"
+    OTHER = "Other"
+    UNEMPLOYED = "Unemployed"
 
 
 class House(models.Model):
-    house_id = models.IntegerField(primary_key=True, validators=[
-                                   MinValueValidator(0)], default=0)
+    house_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     public = models.BooleanField(default=False)
     country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
-    parent_profession_1 = models.CharField(max_length=50, choices=Job.choices,
-                                           default=Job.OTHER)
-    parent_profession_2 = models.CharField(max_length=50, choices=Job.choices,
-                                           default=Job.OTHER)
+    parent_profession_1 = models.CharField(max_length=50, choices=Job.choices, default=Job.OTHER)
+    parent_profession_2 = models.CharField(max_length=50, choices=Job.choices, default=Job.OTHER)
     income = models.IntegerField(null=True, blank=True)
     children = models.IntegerField(null=True, blank=True)
 
